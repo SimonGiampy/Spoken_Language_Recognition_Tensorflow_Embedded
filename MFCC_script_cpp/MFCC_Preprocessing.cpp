3b6f2c524bee34aca47142cc0600a0b47bc2c23f
SimonGiampy <simongiampydev@gmail.com>
2023-08-12 19:20:54 +0200

    preprocessing script adapted for changed library

--- MFCC_script_cpp/MFCC_Preprocessing.cpp
+++ MFCC_script_cpp/MFCC_Preprocessing.cpp
@@ -4,20 +4,29 @@
 // path of the ita and eng dataset to read from
 std::string datasets_path = getCurrentPath() + "datasets\\";
 
-
-
 int main() {
+
+	audio_test = new int16_t[length];
+    readBinary("test_audio.bin");
+	int16_t **audio_test_matrix = reshapeVector(audio_test);
+	delete[] audio_test;  // frees from memory original audio vector
+    compute_mfcc_save(audio_test_matrix);
+	
+
+    /*
+    // create entire dataset of csv files containing the MFCC coefficients
     // create array of languages to elaborate: ita, eng
     std::string languages[2] = {"ita", "eng"};
     for (int i = 0; i < languages->size(); i++) {
         elaborate_dataset(languages[i]);
     }
+    */
 
     return 0;
 }
 
 void elaborate_dataset(std::string lang) {
-// read csv file containing the arrays of integers representing the audio samples
+    // read csv file containing the arrays of integers representing the audio samples
     std::string audio_path = datasets_path + "dataset_" + lang + ".csv";
 
     std::cout << "reading from: " << audio_path << std::endl;
@@ -31,7 +40,6 @@ void elaborate_dataset(std::string lang) {
     }
 
     std::string line;
-    int length = 10 * 16000;  // 10 seconds of audio at 16kHz
     int16_t* dataArray = new int16_t[length];
     int sample_number = 0;
 
@@ -66,7 +74,8 @@ void elaborate_dataset(std::string lang) {
             i++;
         }
 
-        compute_mfcc_save(dataArray, lang, sample_number);
+        compute_mfcc_save(reshapeVector(dataArray), lang, sample_number);
+
         sample_number++;
     }
 
@@ -75,32 +84,55 @@ void elaborate_dataset(std::string lang) {
 
 }
 
-void compute_mfcc_save(int16_t* audio_sample, std::string lang, int sample_number) {
+void compute_mfcc_save(int16_t** audio_sample, std::string lang, int sample_number) {
     std::cout << "computing mfcc " << sample_number << std::endl;
     mymfcc = new arduinoMFCC(num_filters, frame_size, hop_size, length, num_cepstral_coeffs, frequency);
 
     float** mfcc_coeffs = mymfcc->compute(audio_sample);
-    //mymfcc->writeFloatArrayToCSV(mfcc_coeffs, mfcc_path);
-
-    int8_t** quantized_mfcc_coeffs = mymfcc->quantizeMFCC(mfcc_coeffs);
+    
+    int8_t** quantized_mfcc_coeffs = mymfcc->quantizeMFCC();
 
     char formattedString[4 + 1];  // +1 for null-terminator
     snprintf(formattedString, sizeof(formattedString), "%04d", sample_number);
     std::string formatted(formattedString);
-    std::string audio_name = std::string(datasets_path + "mfcc_q_" + lang + "_" + formatted);
+    std::string audio_name = std::string(datasets_path + "mfcc_q_" + lang + "_" + formatted + ".csv");
 
     std::cout << "saving mfcc to: " << audio_name << std::endl;
 
-    mymfcc->writeInt8ArrayToCSV(quantized_mfcc_coeffs, audio_name);
+    writeInt8ArrayToCSV(quantized_mfcc_coeffs, audio_name);
+
+    delete mymfcc;
+}
+
+
+void compute_mfcc_save(int16_t** audio_sample) {
+    std::cout << "computing mfcc " << std::endl;
+
+    mymfcc = new arduinoMFCC(num_filters, frame_size, hop_size, length, num_cepstral_coeffs, frequency);
+
+    float** mfcc_coeffs = mymfcc->compute(audio_sample);
+
+	std::cout << "saving base float mfcc to: mfcc_test_float.csv" << std::endl;
+	writeFloatArrayToCSV(mfcc_coeffs, "mfcc_test_float.csv");
+
+	float** norm_mfcc_coeffs = mymfcc->normalizeMFCC();
+
+	std::cout << "saving base float mfcc to: mfcc_test__norm_float.csv" << std::endl;
+	writeFloatArrayToCSV(mfcc_coeffs, "mfcc_test_norm_float.csv");
+    
+    int8_t** quantized_mfcc_coeffs = mymfcc->quantizeMFCC();
+
+	std::cout << "saving int 8 mfcc to: mfcc_test_int8.csv" << std::endl;
+	writeInt8ArrayToCSV(quantized_mfcc_coeffs, "mfcc_test_int8.csv");
 
     delete mymfcc;
 }
 
 std::string getCurrentPath() {
-    return "D:\\Programming\\Spoken_Language_Recognition_Tensorflow_Embedded\\";
+    //"D:\\Programming\\Spoken_Language_Recognition_Tensorflow_Embedded\\";
+    // return parent path of the executable file created with g++ compiler
+    return "/home/simon/Spoken_Language_Recognition_Tensorflow_Embedded/";
 }
-
-
 
 /**
  * reshapes audio vector into a matrix where each row is one hop, so that the original audio array
@@ -109,19 +141,19 @@ std::string getCurrentPath() {
  * this code is not to be placed on the arduino
  */
 int16_t **reshapeVector(int16_t *vector) {
-    int16_t **matrix = new int16_t *[length / hop_size];
-    for (unsigned int i = 0; i < length / hop_size; i++) {
+	int matrix_height = length / hop_size;
+    int16_t **matrix = new int16_t *[matrix_height];
+    for (unsigned int i = 0; i < matrix_height; i++) {
         matrix[i] = new int16_t[hop_size];
     }
 
     int vecIndex = 0;
-    for (unsigned int i = 0; i < length / hop_size; i++) {
+    for (unsigned int i = 0; i < matrix_height; i++) {
         for (unsigned int j = 0; j < hop_size; j++) {
             matrix[i][j] = vector[vecIndex++];
         }
     }
 
-    //delete[] audio;  // frees from memory original audio vector
     return matrix;
 }
 
@@ -139,9 +171,49 @@ void readBinary(const char *filePath) {
         int16_t valueInt16;
         for (int i = 0; i < seconds * frequency; i++) {
             inputFile.read(reinterpret_cast<char *>(&valueInt16), sizeof(int16_t));
-            //audio[i] = valueInt16;
+            audio_test[i] = valueInt16;
         }
         // Close the file when done
         inputFile.close();
     }
 }
+
+
+void writeInt8ArrayToCSV(int8_t **mfcc_coeffs, std::string csv_name) {
+    // Open a file for writing
+    std::ofstream outFile(csv_name);
+
+    // Write the matrix elements to the CSV file
+    for (int i = 0; i < mfcc_matrix_rows; ++i) {
+        for (int j = 0; j < mfcc_matrix_cols - 1; ++j) {
+            outFile << static_cast<int>(mfcc_coeffs[i][j]) << ", "; // Convert int8 to int before writing
+        }
+        outFile << static_cast<int>(mfcc_coeffs[i][mfcc_matrix_cols-1]) << std::endl;
+    }
+
+    // Close the file
+    outFile.close();
+
+    std::cout << "MFCC quantized coefficients csv file has been created." << std::endl;
+
+}
+
+void writeFloatArrayToCSV(float **mfcc_coeffs, std::string csv_name) {
+    // Open a file for writing
+    std::ofstream outFile(csv_name);
+
+    // Write the matrix elements to the CSV file
+    for (int i = 0; i < mfcc_matrix_rows; ++i) {
+        for (int j = 0; j < mfcc_matrix_cols - 1; ++j) {
+            outFile << mfcc_coeffs[i][j] << ", "; // Convert int8 to int before writing
+        }
+        outFile << mfcc_coeffs[i][mfcc_matrix_cols-1] << std::endl;
+    }
+
+    // Close the file
+    outFile.close();
+
+    std::cout << "MFCC float coefficients csv file has been created." << std::endl;
+
+}
+
--- MFCC_script_cpp/MFCC_Preprocessing.h
+++ MFCC_script_cpp/MFCC_Preprocessing.h
@@ -9,25 +9,36 @@
 const uint8_t num_filters = 40;
 const uint16_t frame_size = 512;
 const uint16_t hop_size = 128;
-const uint8_t num_cepstral_coeffs = 12;
+const uint8_t num_cepstral_coeffs = 13;
 const uint16_t frequency = 16000;
 
 // 10 seconds audio recording at 16kHz at 2 bytes/sample
 const int seconds = 10;
 const unsigned int length = frequency * seconds;
 
-
 // MFCC object
 arduinoMFCC *mymfcc;
+int mfcc_matrix_rows = length / hop_size - (frame_size / hop_size) + 1;
+int mfcc_matrix_cols = num_cepstral_coeffs;
+
+int16_t *audio_test;
 
 void readBinary(const char *filePath);
 
 int16_t **reshapeVector(int16_t *vector);
 
-void compute_mfcc_save(int16_t* audio_sample, std::string lang, int sample_number);
+// computes MFCC, normalize, quantize and save to csv with formatted file name
+void compute_mfcc_save(int16_t** audio_sample, std::string lang, int sample_number);
+// computes MFCC, normalize, quantize, and save to csv as a test file
+void compute_mfcc_save(int16_t** audio_sample);
+
 std::string getCurrentPath(void);
+
+// reads dataset_[lang].csv, computes MFCC for each sample in the dataset and save it to csv files in the datasets folder
 void elaborate_dataset(std::string lang);
    
-
+// saves MFCC matrix into csv file
+void writeInt8ArrayToCSV(int8_t **mfcc_coeffs, std::string csv_name);
+void writeFloatArrayToCSV(float **mfcc_coeffs, std::string csv_name);
 
 int main(void);
